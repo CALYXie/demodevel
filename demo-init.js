7787b530c0447a59c2da3da70803e6b424706de6
Christopher Vella <chris@calyx.net.au>
2014-06-17 11:57:44 +1000

    demo-init.js mod

--- demo-init.js
+++ demo-init.js
@@ -11,22 +11,19 @@ function  getScript2 (url, callback) {
         var done    = false; // Handle Script loading
 
         script.src  = url;
-        script.onload = script.onreadystatechange = function() { // 
-Attach handlers for all browsers
-            if ( !done && (!this.readyState || this.readyState === 
-"loaded" || this.readyState === "complete") ) {
+        script.onload = script.onreadystatechange = function() { // Attach handlers for all browsers
+            if ( !done && (!this.readyState || this.readyState === "loaded" || this.readyState === "complete") ) {
                 done = true;
                 if (callback) { callback(); }
-                script.onload = script.onreadystatechange = null; // 
-Handle memory leak in IE
+                script.onload = script.onreadystatechange = null; // Handle memory leak in IE
             }
         };
 
         head.appendChild(script);
-        return undefined; // We handle everything using the script 
-element injection
+        return undefined; // We handle everything using the script element injection
     };
-// ----------------------------------
+// -----------------------------------------
+
 
 // create a dummy block to inject js
 $("<div></div>").attr('id','calyx-js').appendTo('body');
@@ -34,6 +31,4 @@ $("<div></div>").attr('id','calyx-js').appendTo('body');
 getScript2('https://raw.githubusercontent.com/CALYXie/demodevel/master/init.js');
 
 
-
 });
-
