f80585cc7963c1a082666be3a4d8def42198d36e
Christopher Vella <chris@calyx.net.au>
2014-06-17 11:32:01 +1000

    modified scripts

--- demo-init.js
+++ demo-init.js
@@ -26,64 +26,12 @@ Handle memory leak in IE
         return undefined; // We handle everything using the script 
 element injection
     };
-// -----------------------------------------
-
-
-//alert(location.host);
-
-var re = new RegExp('ebc');
-if (location.host.match(re) ) { host = 'ebc' };
-var re = new RegExp('emmaus.edu.au');
-if (location.host.match(re) ) { host = 'ebc' };
-var re = new RegExp('ausemm.calyx.net.au');
-if (location.host.match(re) ) { host = 'ebc' };
-
-// --------------------
-
-var re = new RegExp('ebchaeng');
-if (location.host.match(re) ) { host = 'ebchaeng' };
-
-var re = new RegExp('library.tabornsw.edu.au');
-if (location.host.match(re) ) { host = 'ebchaeng' };
-
-var re = new RegExp('tcn');
-if (location.host.match(re) ) { host = 'ebchaeng' };
-
-
-// --------------------
-
-var re = new RegExp('ebcsyd');
-if (location.host.match(re) ) { host = 'ebckor' };
-
-var re = new RegExp('kor.calyx.net.au');
-if (location.host.match(re) ) { host = 'ebckor' };
-var re = new RegExp('kor');
-if (location.host.match(re) ) { host = 'ebckor' };
-
-var re = new RegExp('koreanlibrary.scd.edu.au');
-if (location.host.match(re) ) { host = 'ebckor' };
-
-var re = new RegExp('scd.edu');
-if (location.host.match(re) ) { host = 'ebckor' };
-
-// --------------------
-var re = new RegExp('mcm.kohaaloha.com');
-if (location.host.match(re) ) { host = 'mcm' };
-
-var re = new RegExp('library.mcm.qld.edu.au');
-if (location.host.match(re) ) { host = 'mcm' };
-
-var re = new RegExp('mcm.calyx.net.au');
-if (location.host.match(re) ) { host = 'mcm' };
-
-// --------------------
-
-
+// ----------------------------------
 
 // create a dummy block to inject js
 $("<div></div>").attr('id','calyx-js').appendTo('body');
 
-getScript2('/calyx/' + host + '/init.js');
+getScript2('https://raw.githubusercontent.com/CALYXie/demodevel/master/init.js');
 
 
 
--- init.js
+++ init.js
@@ -1,2 +1,37 @@
-$( "#login" ).hide();
-$("#news").appendTo( "#newspaste" );
+document.title = 'Emmaus Bible College Library';
+
+$('#opacmainuserblock').load('/calyx/'+ host +'/main.html');
+
+$('#libraryname').load('/calyx/'+ host +'/logo.html');
+
+// load header
+$('#members').after('<div id="calyx-header"></div>')
+$('#calyx-header').load('/calyx/'+ host +'/header.html');
+
+// load footer
+$('.yui-t1 .ft').remove();
+$('#bd').after('<div id="calyx-ft" class=".ft"></div>')
+$('#calyx-ft').load('/calyx/'+ host +'/footer.html');
+
+
+$('#opacnav').load('/calyx/'+ host +'/opacnav.html');
+
+
+$("#opacnav").css({'font-size' : '80%'});
+
+
+$(document).ready(function(){
+if ($("#logout").length) {
+    $("#moresearches").append("<span class=\"pipe\"> | </span><a href=\"http://www.calyx.net.au/media/EBCebsco.html\"target=\"_blank\" > EBSCO 
+database </a>");
+}});
+$(document).ready(function(){
+if ($("#logout").length) {
+    $("#moresearches").append("<span class=\"pipe\"> | </span><a href=\"http://www.anztla.org/AriLogin.aspx \"target=\"_blank\" > ANZTLA 
+Publication ARI (login: EBCL, pw: suamme) </a>");
+}});
+
+//Auto select branch
+jQuery("select#select_library option[selected]").removeAttr("selected");
+jQuery("select#select_library option[value='branch:MAIN']").attr("selected", "selected");
+
